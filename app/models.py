a8061e970356926c287d5535a1e1053d7289e263
Steve99-coder <stevenderitu99@gmail.com>
2020-12-12 19:22:21 +0300

    Add a class for subscribers with table name subscribers

--- app/models.py
+++ app/models.py
@@ -98,3 +98,16 @@ class Quote:
    self.id=id
    self.author=author
    self.content=content
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
