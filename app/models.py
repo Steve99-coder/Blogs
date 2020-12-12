26dfcdf1c14e5e365e82c84405e39ccf756476f8
Steve99-coder <stevenderitu99@gmail.com>
2020-12-12 19:17:13 +0300

    Add the class for blogs

--- app/models.py
+++ app/models.py
@@ -39,3 +39,28 @@ class User (UserMixin,db.Model):
 
     def __repr__(self):
         return "User: %s" %str(self.username)
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.Text(),nullable=False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_blog(id):
+        blog = Blog.query.filter_by(id=id).first()
+
+        return blog
+
+    def __repr__(self):
+        return f'Blog {self.title}'
